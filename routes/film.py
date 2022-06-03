5f24c616012f43367925ad92c1e979712c146e8b
Alejandro Diaz <alejandro.diaz@axians.es>
2022-06-03 14:54:48 +0200

    ADD routes template for films requests

---
+++ routes/film.py
@@ -0,0 +1,29 @@
+from fastapi import APIRouter
+
+film = APIRouter()
+
+#give a welcome message to the user
+@film.get("/films/")
+def welcome():
+    return {"message": "Welcome to the film API"}
+
+#give a welcome message to the user
+@film.get("/films/")
+def welcome():
+    return {"message": "Welcome to the film API"}
+
+#give a welcome message to the user
+@film.get("/films/")
+def welcome():
+    return {"message": "Welcome to the film API"}
+
+#give a welcome message to the user
+@film.get("/films/")
+def welcome():
+    return {"message": "Welcome to the film API"}
+
+#give a welcome message to the user
+@film.get("/films/")
+def welcome():
+    return {"message": "Welcome to the film API"}
+
