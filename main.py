2302bb80578bc558b67eb66240550d0924b0eb7c
Alejandro Diaz <alejandro.diaz@axians.es>
2022-06-08 16:50:12 +0200

    ADD description and title

--- main.py
+++ main.py
@@ -3,7 +3,29 @@ from routes.film import film
 from routes.rental import rental
 from errors.errors import exception_handler_wrapper
 
-app = FastAPI()
+description = """
+## Virtual-video rental service includes two routes:
+
+### Films
+
+* Get all films
+* Get all films by title coincidence
+* Create films
+* Delete a film by id
+* Update a film by id
+
+### Rental
+
+* Check if a film is available for rent
+* Rent a film
+* Return a film
+
+"""
+
+app = FastAPI(
+    title="Virtual-video Rental Service API",
+    description=description,
+)
 
 app.include_router(film)
 app.include_router(rental)
